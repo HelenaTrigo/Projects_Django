3a1e6ff7884ab83e98d9d5741dcf6a473faf546e
Helena Trigo <trigoh91@gmail.com>
2024-01-31 12:12:17 +0000

    Criação e registo do modelo (BD)

--- project1/lista_compras/admin.py
+++ project1/lista_compras/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from project1.lista_compras.models import Artigo
 
 # Register your models here.
+admin.site.register(Artigo)
--- project1/lista_compras/models.py
+++ project1/lista_compras/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Artigo(models.Model):
+
+    OPCOES = [
+        ('U', 'Urgente'),
+        ('N', 'Não Urgente')
+    ]
+
+    nome = models.CharField(max_length = 252)
+    quantidade = models.CharField(max_length = 20, default = '1')
+    urgente = models.CharField(max_length = 1, choices = OPCOES, default = 'N')
