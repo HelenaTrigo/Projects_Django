189a4ad3fa9c225ddf333168ac83d9fb5135fdd0
Helena Trigo <trigoh91@gmail.com>
2024-02-05 12:00:55 +0000

    função alterar, correção de erro, levantamento de excepções para segunrança do código.

--- project1/contactos/urls.py
+++ project1/contactos/urls.py
@@ -6,5 +6,6 @@ app_name = 'contactos'
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('add/', views.add, name='add'),
-    path('delete/<int:id>', views.delete, name = 'delete')
+    path('delete/<int:id>', views.delete, name = 'delete'),
+    path('edit/<int:id>', views.edit, name = 'edit'),
 ]
--- project1/contactos/views.py
+++ project1/contactos/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from contactos.models import Contacto
 from contactos.forms import ContactoForm
@@ -12,6 +13,7 @@ def home(request):
 def add(request):
     formulario = ContactoForm()
     if request.method == "POST":
+        # request files permite o upload de imagens
         formulario = ContactoForm(request.POST, request.FILES)
         if formulario.is_valid():
             formulario.save()
@@ -21,4 +23,21 @@ def add(request):
 def delete(reqeust, id):
     contacto = Contacto.objects.filter(pk = id)
     contacto.delete()
-    return redirect('contacto:home')
+    return redirect('contactos:home')
+
+def edit(request, id):
+    try:
+        contacto = Contacto.objects.get(pk = id)
+    except:
+        return HttpResponse("Erro: o contacto não existe. Será redircionado para nova página em 5 segundos.")
+        # em html redirediona para pagina no tempo definido <meta http-equiv="refresh" content="5; URL={% url 'contactos:home' %}"/>
+       
+    else:
+        # Cria formulario com a informação antiga do contacto
+        formulario = ContactoForm(instance = contacto)
+        if request.method == 'POST':
+            # tem que referir a instancia do formualario (registo) para não inserir um novo
+            formulario = ContactoForm(request.POST, request.FILES, instance = contacto)
+            formulario.save()
+            return redirect('contactos:home')
+        return render(request, 'contactos/edit.html', {'formulario':formulario})
